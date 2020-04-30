d27eaa16a759c3fc7c3b61259fe3284ae583757c
yutasb <shibamoto.yuta@gmail.com>
2020-04-30 12:28:40 +0900

    textareaのスタイル編集

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,10 +1,26 @@
 class MypagesController < ApplicationController
   def edit
-    @user = current_user.id
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to mypage_path(@user), notice: '更新しました'
+    else
+      flash[:alert] = '更新できませんでした'
+      render :edit
+    end
   end
 
   def show
     @user = current_user.id
     @wishes = Wish.where(user_id: current_user)
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:myself)
+  end
 end
+
